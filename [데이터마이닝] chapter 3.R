90937245663711ef6fa91fcb942756887393cd3d
oneoclockc <eho6119vn@naver.com>
2022-04-20 17:51:39 +0900

    ~ logistic regression

--- [데이터마이닝] chapter 3.R
+++ [데이터마이닝] chapter 3.R
@@ -117,3 +117,106 @@ fit <- lda(Spcieds~., data=iris12.train)
 pred<-predict(fit, newdata=iris12[-train,])$class
 table(pred,iris12[-train,3])
 mean(pred!=iris12[-train,3])
+
+
+
+
+# 6.0.1. naive bayes classifier for iris data
+library(e1071)
+fit <-naiveBayes(Species~., data=iris.train)
+pred.iris <-predict(fit, iris.test)
+table(pred=pred.iris, true = iris.test[,5])
+iris.test[,5]
+
+
+# two discretized and two continuous predictors of iris data
+
+head(iris)
+nirisf.train <- irisf.train
+nirisf.train
+
+nirisf.train$Petal.Length <-iris.train$Petal.Length
+nirisf.train$Petal.Width <-iris.train$Petal.Width
+nirisf.test <- irisf.test
+nirisf.test$Petal.Length <-iris.test$Petal.Length
+nirisf.test$Petal.Width <-iris.test$Petal.Width
+fitn <- naiveBayes(Y~., data = nirisf.train)
+pred.nirisf <-predict(fitn,nirisf.test)
+table(pred = pred.nirisf, true=nirisf.test$Y)
+
+mean(pred.nirisf != nirisf.test$Y)
+
+
+
+# logistic regression
+irisb <- iris[iris$Species != "setosa", c(1,2,5)] # setosa 제외, versicolor랑 virginica만 남김 
+head(irisb)
+
+plot(irisb[,1:2], col=irisb[,3])
+
+irisb$Y<- rep(0,dim(irisb)[1]) 
+irisb$Y
+irisb$Y[irisb$Species == "virginica"] <-1  # virginica 는 1로, versicolor은 0으로 코딩 
+irisb$Y  
+
+head(iris)
+
+n<-dim(iris)[1]
+n
+set.seed(1)
+train
+train <- sample(n,n/2)
+fit <-glm(Y~Sepal.Length, data = irisb[train,],family = binomial) # 모델 적합 
+summary(fit)
+
+b<-c(coef(fit))
+b # 모델 계수 확인 
+dim(irisb[train,])
+pred <- predict(fit, newdata = irisb[-train,],type = "response")
+pred # 1에 가까운 것 -> virginica, 0에 가까운 것 -> versicolor  
+pcls <- round(pred)
+pcls
+mean(pcls != irisb$Y[-train]) # test error 
+
+
+
+with(irisb,plot(Sepal.Length,Y)) # verinica가 대체적으로 sepal.length가 크다 
+curve(exp(b[1]+b[2]*x)/(1+exp(b[1]+b[2]*x)), col=2, add=T)
+
+b
+
+library(ISLR)
+head(Default)
+str(Default)
+
+fit <-glm(default~balance, data= Default, family = binomial)
+summary(fit)
+
+
+data.frame(student = c("yes","no"), balance=rep(1500,2), income = rep(40000,2)) # 더미데이터? 
+predict(fit,data.frame(student =c("Yes","No"), balance=rep(1500,2), income = rep(40000,2)), type = "response")
+
+# student -> default 일 확률 높음 
+
+
+
+# multinomial logistic regression
+n = dim(iris)[1]
+set.seed(1)
+train = sample(n,n/2)
+iris.train=iris[train,]
+iris.test=iris[-train,]
+
+library(nnet)
+fitm = multinom(Species~., data= iris.train)
+summary(fitm)
+
+predm = predict(fitm, iris.test)
+predm
+
+mean(predm == iris.test$Species) # test 정확도
+predprob = predict(fitm, iris.test, type = "prob")
+head(predprob) # 각 종별 확률 
+
+library(caret)
+confusionMatrix(predm, iris.test$Species) # 모델 정확도, 통계량 등 세부적인 정보
